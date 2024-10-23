984a95930a6632c473ca204b110bd0cd2b2dd74b
Duy Linh <22520762@gm.uit.edu.vn>
2024-10-23 15:56:23 +0700

    More_fragment

--- app/src/main/java/com/example/piechart/uidn/more/MoreFragment.java
+++ app/src/main/java/com/example/piechart/uidn/more/MoreFragment.java
@@ -1,10 +1,12 @@
 package com.example.piechart.uidn.more;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -12,6 +14,8 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.piechart.Activity.ChooseActivity;
+import com.example.piechart.Custom.CustomAdapter;
 import com.example.piechart.databinding.FragmentHomeBinding;
 import com.example.piechart.databinding.FragmentHomeDnBinding;
 import com.example.piechart.databinding.FragmentMoreBinding;
@@ -27,13 +31,43 @@ import java.util.ArrayList;
 public class MoreFragment extends Fragment {
 
     private FragmentMoreBinding binding;
-
+    ListView listView;
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         binding = FragmentMoreBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        Button btnBack = binding.btnBack;
+
+        // Xử lý sự kiện khi nhấn button
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Chuyển sang Activity khác
+                Intent intent = new Intent(getActivity(), ChooseActivity.class);
+                startActivity(intent);
+                // Optional: kết thúc Fragment hoặc Activity hiện tại
+                getActivity().finish();
+            }
+        });
+
+        //final TextView textView = binding.textNotifications;
+        //notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        listView = binding.ListCN;
+
+        // Dữ liệu cho ListView
+        ArrayList<String> listItems = new ArrayList<>();
+        listItems.add("Giới thiêu bạn bè");
+        listItems.add("Đánh giá");
+        listItems.add("Thông tin nhóm");
+        listItems.add("Cài đặt");
+
+        // Tạo Adapter và gán cho ListView
+        CustomAdapter adapter = new CustomAdapter(getContext(), listItems);
+        listView.setAdapter(adapter);
+
+        listView.setAdapter(adapter);
         return root;
     }
 
