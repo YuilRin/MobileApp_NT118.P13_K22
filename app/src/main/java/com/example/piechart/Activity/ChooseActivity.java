951bc2d4a3c80edff3c4f13ae665986625df48c1
Duy Linh <22520762@gm.uit.edu.vn>
2024-10-23 09:26:42 +0700

    Navi_Back in ChooseActivity

--- app/src/main/java/com/example/piechart/Activity/ChooseActivity.java
+++ app/src/main/java/com/example/piechart/Activity/ChooseActivity.java
@@ -2,10 +2,13 @@ package com.example.piechart.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.piechart.R;
 
@@ -14,18 +17,37 @@ public class ChooseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose);
-        Button button=findViewById(R.id.individual);
 
-        button.setOnClickListener(new View.OnClickListener()
-        {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        // Thiết lập Toolbar tùy chỉnh
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(R.drawable.baseline_arrow_back_24);
+
+        // Đặt tiêu đề cho toolbar
+        toolbar.setTitle("Choose");
+
+        // Xử lý sự kiện khi nhấn vào nút quay lại
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainIntent = new Intent(ChooseActivity.this, LoginActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        });
+
+        // Xử lý sự kiện khi nhấn vào button
+        Button button = findViewById(R.id.individual);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent mainIntent = new Intent(ChooseActivity.this, MainActivity.class);
                 startActivity(mainIntent);
                 finish();
-
             }
-
         });
     }
 }
