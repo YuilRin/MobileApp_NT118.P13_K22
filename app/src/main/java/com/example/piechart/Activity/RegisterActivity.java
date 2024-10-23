8cbe3085c563d3adc9570fbf62a287406e484665
RibunLoc <hothanhloc12345@gmail.com>
2024-10-23 21:06:30 +0700

    Bổ sung activity cho giao diện

---
+++ app/src/main/java/com/example/piechart/Activity/RegisterActivity.java
@@ -0,0 +1,32 @@
+package com.example.piechart.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.piechart.R;
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        ImageView img_Exit = findViewById(R.id.exit);
+
+        img_Exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mainIntent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(mainIntent);
+                finish();
+
+            }
+        });
+
+
+    }
+
+}
