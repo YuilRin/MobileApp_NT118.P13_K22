b94fed06849259540ce2dccd14f08121f6fde228
kPhuong08 <22521170@gm.uit.edu.vn>
2024-10-28 02:17:53 +0700

    update file name

--- app/src/main/java/com/example/piechart/uidn/QuanLy/QuanLyFragment.java
+++ app/src/main/java/com/example/piechart/uidn/QuanLy/QuanLyFragment.java
@@ -1,34 +1,25 @@
 package com.example.piechart.uidn.QuanLy;
 
 import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.viewpager.widget.ViewPager;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
-import com.example.piechart.Custom.CustomAdapter_Money;
+
 import com.example.piechart.R;
-import com.example.piechart.databinding.BusinessFragmentHomeBinding;
 import com.example.piechart.databinding.BusinessFragmentQuanlyBinding;
-import com.example.piechart.uidn.TabLayoutFragment.AllOrdersFragment;
-import com.example.piechart.uidn.TabLayoutFragment.OtherOrdersFragment;
-import com.example.piechart.uidn.TabLayoutFragment.PaidOrdersFragment;
 import com.example.piechart.uidn.TabLayoutFragment.QuanLy.BusinessQuanLyAll;
 import com.example.piechart.uidn.TabLayoutFragment.QuanLy.BusinessQuanLyKhoanChi;
 import com.example.piechart.uidn.TabLayoutFragment.QuanLy.BusinessQuanLyKhoanThu;
-import com.example.piechart.uidn.TabLayoutFragment.UnpaidOrdersFragment;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
-import java.util.ArrayList;public class QuanLyFragment extends Fragment {
+public class QuanLyFragment extends Fragment {
 
     private BusinessFragmentQuanlyBinding binding;
 
@@ -90,7 +81,7 @@ import java.util.ArrayList;public class QuanLyFragment extends Fragment {
     private void showKhoanThuDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
         builder.setTitle("Khoản Thu");
-        View view = LayoutInflater.from(getContext()).inflate(R.layout.business_item_khoanthu, null);
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.business_quanly_edit_khoanthu_item, null);
         builder.setView(view);
         builder.setPositiveButton("Save", (dialog, which) -> {
             // Code xử lý khi nhấn Save
@@ -102,7 +93,7 @@ import java.util.ArrayList;public class QuanLyFragment extends Fragment {
     private void showKhoanChiDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
         builder.setTitle("Khoản Chi");
-        View view = LayoutInflater.from(getContext()).inflate(R.layout.business_item_khoanchi, null);
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.business_quanly_edit_khoanchi_item, null);
         builder.setView(view);
         builder.setPositiveButton("Save", (dialog, which) -> {
             // Code xử lý khi nhấn Save
