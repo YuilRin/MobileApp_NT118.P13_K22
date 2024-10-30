1ed8df40535af0cb7261feb3b86da6e6eeec93ab
kPhuong08 <22521170@gm.uit.edu.vn>
2024-10-31 01:00:12 +0700

    update edit item

--- app/src/main/java/com/example/piechart/Activity/Main_DN_Activity.java
+++ app/src/main/java/com/example/piechart/Activity/Main_DN_Activity.java
@@ -9,7 +9,6 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.piechart.R;
-master
 
 import com.example.piechart.databinding.BusinessActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -21,7 +20,6 @@ public class Main_DN_Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-master
 
 
         com.example.piechart.databinding.BusinessActivityMainBinding binding = BusinessActivityMainBinding.inflate(getLayoutInflater());
