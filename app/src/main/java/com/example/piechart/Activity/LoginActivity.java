50c414319a087972659561d657a80bbebabb9648
Duy Linh <22520762@gm.uit.edu.vn>
2024-10-31 14:05:30 +0700

    Change Activity to Fragment

--- app/src/main/java/com/example/piechart/Activity/LoginActivity.java
+++ app/src/main/java/com/example/piechart/Activity/LoginActivity.java
@@ -1,37 +1,24 @@
 package com.example.piechart.Activity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.piechart.Activity.LoginFragment.LoginFragment;
 import com.example.piechart.R;
 
 public class LoginActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Button button=findViewById(R.id.login);
-        Button btn_ttk=findViewById(R.id.btn_ttk);
-        button.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v) {
-                    Intent mainIntent = new Intent(LoginActivity.this, ChooseActivity.class);
-                    startActivity(mainIntent);
-                    finish();
-            }
-        });
 
-        btn_ttk.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v) {
-                Intent mainIntent = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(mainIntent);
-                finish();
-            }
-        });
+        // Kiểm tra nếu chưa có Fragment nào trong container thì thêm LoginFragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new LoginFragment())
+                    .commit();
+        }
     }
 }
---
+++ app/src/main/java/com/example/piechart/Activity/LoginFragment/ChooseFragment.java
@@ -0,0 +1,74 @@
+package com.example.piechart.Activity.LoginFragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.example.piechart.Activity.MainDnActivity;
+import com.example.piechart.Activity.MainActivity;
+import com.example.piechart.R;
+public class ChooseFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_choose, container, false);
+        setHasOptionsMenu(true);
+        // Xử lý sự kiện khi nhấn vào nút "individual"
+        Button individualButton = view.findViewById(R.id.individual);
+        individualButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainIntent = new Intent(getActivity(), MainActivity.class);
+                startActivity(mainIntent);
+                getActivity().finish();
+            }
+        });
+
+        // Xử lý sự kiện khi nhấn vào nút "btn_dn"
+        Button businessButton = view.findViewById(R.id.btn_dn);
+        businessButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainIntent = new Intent(getActivity(), MainDnActivity.class);
+                startActivity(mainIntent);
+                getActivity().finish();
+            }
+        });
+
+        return view;
+    }
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        // Ẩn nút quay lại khi fragment bị hủy
+        if (getActivity() != null) {
+            ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        }
+    }
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        // Hiển thị nút quay lại trên ActionBar
+        if (getActivity() != null) {
+            ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            // Xử lý quay lại khi nhấn nút Back trên ActionBar
+            getParentFragmentManager().popBackStack();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
